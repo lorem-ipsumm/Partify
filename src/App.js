42b1efaca4291ed3cf2fd57d2a231d6f68c07eb4
Solar <Nicholasmrnickmorris@gmail.com>
2018-03-25 00:23:07 -0400

    escelators

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './css/main.css';
 
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Party from './pages/Party.js';
 
@@ -9,8 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="main-wrapper">
-        <Route exact path="/" component={Home} />
-        <Route path="/party" component={Party} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/party" component={Party} />
+        </Switch>
       </div>
     );
   }
